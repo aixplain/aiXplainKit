c80cd5a8dd323b177acf72c06b7ad5f6b50af825
JP Maia <maiajp2305@gmail.com>
2024-03-28 13:15:21 -0300

    Adding Headers in the files

--- Sources/aiXplainKit/Errors/Pipeline+Error.swift
+++ Sources/aiXplainKit/Errors/Pipeline+Error.swift
@@ -1,40 +1,56 @@
-//
-//  File.swift
-//  
-//
-//  Created by Joao Pedro Monteiro Maia on 19/03/24.
-//
+/*
+ AiXplainKit Library.
+ ---
+ 
+ aiXplain SDK enables Swift programmers to add AI functions
+ to their software.
+ 
+ Copyright 2024 The aiXplain SDK authors
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ AUTHOR: João Pedro Maia
+ */
 
 import Foundation
 
 enum PipelineError: Error, Equatable {
     /// No API key was provided for making API calls.
     case missingAPIKey
-
+    
     /// No backend URL was provided for the backend service.
     case missingBackendURL
-
+    
     /// The provided URL is malformed.
     case invalidURL(url: String?)
-
+    
     /// Error during the recoding of model.run response while schedulling the run.
     case failToDecodeRunResponse
-
+    
     /// This error is thrown when the model is polling the response for the job created at `Model.run` did not receive a response/output in the desired time.
     case pollingTimeoutOnModelResponse(pollingURL: URL)
-
+    
     /// Fail to decode ModelOutput during the polling phase.
     case failToDecodeModelOutputDuringPollingPhase(error: String?)
-
+    
     /// Error reported by the supplier or service.
     case supplierError(error: String)
-
+    
     /// An unsupported value type was encountered during input payload generation.
     case typeNotRecognizedWhileCreatingACombinedInput
-
+    
     /// An error occurred during input encoding.
     case inputEncodingError
-
+    
     var localizedDescription: String {
         switch self {
         case .missingAPIKey:
